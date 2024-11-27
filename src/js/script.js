79a18a284f04e8c8b1399b34e9075826c041e2e7
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2024-11-27 11:36:20 +0100

    faire apparaitre le texte tout en fermant celui qui était ouvert

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
 // *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+
+accordeon.addEventListener("click" ,() => {
+    accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+})
